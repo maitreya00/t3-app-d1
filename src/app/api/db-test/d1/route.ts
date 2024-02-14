3d01080a02a4f008d5947fae21b19562e32f3874
maitreya00 <maitreya@xoxo.wtf>
2024-02-15 04:58:49 +0530

    update build command

--- src/app/api/db-test/d1/route.ts
+++ src/app/api/db-test/d1/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
   unstable_noStore();
 
   const t0 = Date.now();
-  const res = await api.dbTest.allProducts.query();
+  const res = await api.dbTest.allProductsD1.query();
   const t1 = Date.now();
   return Response.json({ time: t1 - t0, products: res });
 }
