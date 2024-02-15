7bb667dca3b8f5000b7cacd39b7acbaa0cab9a39
maitreya00 <maitreya@xoxo.wtf>
2024-02-15 08:27:28 +0530

    try fix planescale error: 2

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,14 +1,33 @@
 import { drizzle as drizzleD1 } from "drizzle-orm/d1";
 import { drizzle as drizzlePlanetscale } from "drizzle-orm/planetscale-serverless";
-import { Client } from "@planetscale/database";
+import { Client, connect } from "@planetscale/database";
 import * as sqliteSchema from "./schema-sqlite";
 import * as mysqlSchema from "./schema-mysql";
 
 export const dbD1 = drizzleD1(process.env.DB, { schema: sqliteSchema });
 
-export const dbPlanetscale = drizzlePlanetscale(
-  new Client({
-    url: process.env.PLANETSCALE_DB_URL,
-  }).connection(),
-  { schema: mysqlSchema },
-);
+const regex = /mysql:\/\/([^:]+):([^@]+)@([^/]+)/;
+const matches = (process.env.PLANETSCALE_DB_URL ?? "").match(regex);
+if (!matches) throw new Error("Invalid PLANETSCALE_DB_URL");
+
+const config = {
+  username: matches[1],
+  password: matches[2],
+  host: matches[3],
+  fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    delete (init as any).cache; // Remove cache header
+    return fetch(url, init);
+  },
+};
+// @ts-expect-error test
+const conn = connect(config);
+export const dbPlanetscale = drizzlePlanetscale(conn, { schema: mysqlSchema });
+
+//
+// export const dbPlanetscale = drizzlePlanetscale(
+//   new Client({
+//     url: process.env.PLANETSCALE_DB_URL,
+//   }).connection(),
+//   { schema: mysqlSchema },
+// );
