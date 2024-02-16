ba3504c1ac77883b495cffc6f5bdde2a777067c4
maitreya00 <maitreya@xoxo.wtf>
2024-02-17 04:43:15 +0530

    try fix planetscale

--- src/app/api/db-test/planetscale/route.ts
+++ src/app/api/db-test/planetscale/route.ts
@@ -6,8 +6,25 @@ export const runtime = "edge";
 export async function GET() {
   unstable_noStore();
 
+  const regex = /mysql:\/\/([^:]+):([^@]+)@([^/]+)/;
+  const matches = (process.env.PLANETSCALE_DB_URL ?? "").match(regex);
+  if (!matches) throw new Error("Invalid PLANETSCALE_DB_URL");
+  const config = {
+    host: matches[3],
+    username: matches[1],
+    password: matches[2],
+  };
+
   const t0 = Date.now();
-  const res = await api.dbTest.allProductsPlanetscale.query();
+  let out;
+  try {
+    const res = await api.dbTest.allProductsPlanetscale.query();
+    out = res;
+  } catch (e) {
+    console.error(e);
+    out = JSON.stringify(e);
+  }
+
   const t1 = Date.now();
-  return Response.json({ time: t1 - t0, products: res });
+  return Response.json({ time: t1 - t0, out, config });
 }
