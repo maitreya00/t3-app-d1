d8a9f0e179a7bdd7321ba33adaeebf9c2a2a323b
maitreya00 <maitreya@xoxo.wtf>
2024-02-17 13:41:02 +0530

    try planetscale

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -68,11 +68,11 @@ export const dbPlanetscale = drizzlePlanetscale(
   new Client({
     url: process.env.PLANETSCALE_DB_URL,
     // @ts-expect-error asd
-    fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
-      // eslint-disable-next-line
-      delete (init as any).cache; // Remove cache header
-      return fetch(url, init);
-    },
+    // fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
+    // eslint-disable-next-line
+    // delete (init as any).cache; // Remove cache header
+    // return fetch(url, init);
+    // },
   }).connection(),
   { schema: mysqlSchema },
 );
