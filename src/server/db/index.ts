b03733cbd05a5ae0d0841cd8832fe02e8363ad01
maitreya00 <maitreya@xoxo.wtf>
2024-02-17 13:28:20 +0530

    try fix planetscale

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -34,43 +34,45 @@ const regex = /mysql:\/\/([^:]+):([^@]+)@([^/]+)/;
 const matches = (process.env.PLANETSCALE_DB_URL ?? "").match(regex);
 if (!matches) throw new Error("Invalid PLANETSCALE_DB_URL");
 
-// const config = {
-//   username: matches[1],
-//   password: matches[2],
-//   host: matches[3],
-//   fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
-//     // eslint-disable-next-line
-//     delete (init as any).cache; // Remove cache header
-//     return fetch(url, init);
-//   },
-// };
-//
-// const conn = connect(config);
 const config = {
-  host: matches[3],
   username: matches[1],
   password: matches[2],
-  // eslint-disable-next-line
-  fetch: (url: string, init: any) => {
+  host: matches[3],
+  fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
     // eslint-disable-next-line
-    delete init["cache"];
-    // eslint-disable-next-line
+    delete (init as any).cache; // Remove cache header
     return fetch(url, init);
   },
 };
 
 const conn = connect(config);
+// const config = {
+//   host: matches[3],
+//   username: matches[1],
+//   password: matches[2],
+//   // eslint-disable-next-line
+//   fetch: (url: string, init: any) => {
+//     // eslint-disable-next-line
+//     delete init["cache"];
+//     // eslint-disable-next-line
+//     return fetch(url, init);
+//   },
+// };
+
+// const conn = connect(config);
 
 // export const dbPlanetscale = drizzlePlanetscale(conn, { schema: mysqlSchema });
 //
 export const dbPlanetscale = drizzlePlanetscale(
-  new Client({
-    url: process.env.PLANETSCALE_DB_URL,
-    //   fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
-    //     // eslint-disable-next-line
-    //     delete (init as any).cache; // Remove cache header
-    //     return fetch(url, init);
-    //   },
-  }).connection(),
+  // @ts-expect-error assd
+  new Client(config).connection(),
+  // new Client({
+  //   url: process.env.PLANETSCALE_DB_URL,
+  //     fetch: (url: string, init: RequestInit<RequestInitCfProperties>) => {
+  //       // eslint-disable-next-line
+  //       delete (init as any).cache; // Remove cache header
+  //       return fetch(url, init);
+  //     },
+  // }).connection(),
   { schema: mysqlSchema },
 );
