a4ef85ab8b761877d29649b2dc03e45dd9cc375f
maitreya00 <maitreya@xoxo.wtf>
2024-02-17 11:25:09 +0530

    checking planetscale again!

--- src/app/api/db-test/planetscale/route.ts
+++ src/app/api/db-test/planetscale/route.ts
@@ -12,22 +12,24 @@ export const preferredRegion = ["bom1"];
 export async function GET() {
   unstable_noStore();
 
-  const regex = /mysql:\/\/([^:]+):([^@]+)@([^/]+)/;
-  const matches = (process.env.PLANETSCALE_DB_URL ?? "").match(regex);
-  if (!matches) throw new Error("Invalid PLANETSCALE_DB_URL");
-  const config = {
-    host: matches[3],
-    username: matches[1],
-    password: matches[2],
-    fetch: (url: any, init: any) => {
-      delete init.cache;
-      return fetch(url, init);
-    },
-  };
-  const connection = connect(config);
+  // const regex = /mysql:\/\/([^:]+):([^@]+)@([^/]+)/;
+  // const matches = (process.env.PLANETSCALE_DB_URL ?? "").match(regex);
+  // if (!matches) throw new Error("Invalid PLANETSCALE_DB_URL");
+  // const config = {
+  //   host: matches[3],
+  //   username: matches[1],
+  //   password: matches[2],
+  //   fetch: (url: any, init: any) => {
+  //     delete init.cache;
+  //     return fetch(url, init);
+  //   },
+  // };
+  // const connection = connect(config);
+  //
+  // const t0 = Date.now();
+  // const data = await connection.execute("SELECT * FROM product;");
+  // const t1 = Date.now();
 
-  const t0 = Date.now();
-  const data = await connection.execute("SELECT * FROM product;");
   let out1, out2, err1, err2;
   // try {
   //   const res1 = await api.dbTest.allProductsPlanetscale.query();
@@ -36,22 +38,12 @@ export async function GET() {
   //   console.error(e);
   //   err1 = JSON.stringify(e);
   // }
-  // try {
-  //   const res2 = await dbPlanetscale.query.products.findMany();
-  //   out2 = res2;
-  // } catch (e) {
-  //   console.error(e);
-  //   err2 = JSON.stringify(e);
-  // }
-
+  const t0 = Date.now();
+  const data = await dbPlanetscale.query.products.findMany();
   const t1 = Date.now();
+
   return Response.json({
     time: t1 - t0,
-    // out1: out1 ?? "NONE",
-    // out2: out2 ?? "NONE",
-    // err1,
-    // err2,
-    config,
-    anotherOut: data.rows,
+    products: data,
   });
 }
